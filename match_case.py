31e930eef23e446c9017cb04b9843e80f0d407f9
Darlene-13 <darlenenasimiyu@gmail.com>
2025-07-17 12:41:53 +0300

    Corrected the game with a while and if loop

--- match_case.py
+++ match_case.py
@@ -1,12 +1,28 @@
 import random
 
-random.randint(1,10)   # THis will generate random integers between 1 and 10
-UserInput = input("Enter a any random number:")
+secret_number = random.randint(1,10)   # THis will generate random integers between 1 and 10
+guess_count = 0 
 
-match UserInput:
-    case UserInput if UserInput.isdigit() and 1 <= int(UserInput) <= 10:
-        print(f"Congradulations!! You entered a valid number: {UserInput}")
-    case UserInput if UserInput.isdigit() < 1:
-        print("Ooops your guess is a bit too low, try again!")
-    case UserInput if not UserInput.isdigit() > 10:
-        print("Ooops your guess is a bit too high, try again!")
+while True:
+    user_input = input("Enter a any random number between 1 and 10:")
+
+    #Match case statement to validate user input
+    match user_input:
+        case _ if user_input.isdigit():
+            number = int(user_input)
+            guess_count +=1
+
+            if 1 <= number <=10:
+                if number == secret_number:
+                    print(f"Congratulations! You entered a valid number: {number}")
+                    print(" Wow you guesses the correct number!")
+                    print(f"You guessed it in {guess_count} attempts.")
+                    break
+                elif number < secret_number:
+                    print("Your guess it too low, try again!")
+                else:
+                    print("Your guess it too high, try again!")
+            else:
+                print("Please enter a number between 1 and 10.")
+        case _:
+            print("Invalid input please guess a number between 1 and 10.")
